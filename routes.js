2e03af985c788d0aff9bd4226851a8455aff6288
ChinhTQ <chinhtq@saisystem.vn>
2022-08-01 14:18:30 +0700

    update assignment

--- routes.js
+++ routes.js
@@ -3,26 +3,16 @@ const fs = require('fs');
 const handleRequestListener = (req,res)=>{
     const body = [];
     res.setHeader('Conten-type','form');
-    res.write('<html>');
-    res.write('<body>');
 
     switch(req.url){
         case '/': 
-            res.write('<h1>Welcome my page!</h1>');
-            res.write('<form action="/create-user" method="POST"><button type="submit">Register</button></form>');
-            res.write('<form action="/users" method="POST"><button type="submit">List User</button></form>');
+            readFiles('./html/app.html',res);
             break;
         case '/users':
-            res.write('<ul><li>Tao Quang Chinh</li></ul>');
-            res.write('<ul><li>Doan Van Manh</li></ul>');
-            res.write('<ul><li>Nguyen Dang Vinh</li></ul>');
-            res.write('<ul><li>Luu Quang Luan</li></ul>');
+            readFiles('./html/list_user.html',res);
             break;
         case '/create-user':
-            res.write('<form action="/create-user" method="POST">');
-            res.write('<input type="text" name="message">');
-            res.write('<button type="submit">Register</button>');
-            res.write('</form>');
+            readFiles('./html/input_form.html',res);
             if(req.method === 'POST'){
                 req.on('data',(chunk)=>{
                     body.push(chunk);
@@ -30,14 +20,19 @@ const handleRequestListener = (req,res)=>{
                 req.on('end',()=>{
                     const parsedBody = Buffer.concat(body).toString();
                     const message = parsedBody.split('=')[1];
-                    fs.writeFileSync('data_register.txt',`${message}`);
+                    fs.writeFile('data_register.txt',`${message}`,(err)=>{
+                        if(err) throw err;
+                    });
                 });
             }
             break;
     }
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
 }
+
+const readFiles = (path,res) => fs.readFile(path,(err,data)=>{
+    if(err) throw err;
+    res.write(data);
+    res.end();
+});
 
 module.exports = handleRequestListener;
