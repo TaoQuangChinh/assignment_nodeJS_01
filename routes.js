b3a05055e9f1016fef0bf333ac4a445a84a5707d
ChinhTQ <chinhtq@saisystem.vn>
2022-08-01 15:47:28 +0700

    update assignment

--- routes.js
+++ routes.js
@@ -1,38 +1,40 @@
 const fs = require('fs');
+const url = require('url');
+var body = [];
 
 const handleRequestListener = (req,res)=>{
-    const body = [];
+    const urlParse = url.parse(req.url,true);
+    const paths = req.url === '/' ? './html/app.html' : `./html${urlParse.pathname}`;
     res.setHeader('Conten-type','form');
 
-    switch(req.url){
-        case '/': 
-            readFiles('./html/app.html',res);
-            break;
-        case '/users':
-            readFiles('./html/list_user.html',res);
-            break;
-        case '/create-user':
-            readFiles('./html/input_form.html',res);
-            if(req.method === 'POST'){
-                req.on('data',(chunk)=>{
-                    body.push(chunk);
-                });
-                req.on('end',()=>{
-                    const parsedBody = Buffer.concat(body).toString();
-                    const message = parsedBody.split('=')[1];
-                    fs.writeFile('data_register.txt',`${message}`,(err)=>{
-                        if(err) throw err;
-                    });
-                });
-            }
-            break;
-    }
+    fs.readFile(paths,(err,data)=>{
+        if(err) handleEventError(res);
+        res.write(data);
+        handleWriteFile(req);
+        return res.end();
+    });
 }
 
-const readFiles = (path,res) => fs.readFile(path,(err,data)=>{
-    if(err) throw err;
-    res.write(data);
-    res.end();
-});
+const handleWriteFile = req =>{
+    if(req.url === '/create-user.html' && req.method === 'POST'){
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('data_register.txt',`${message}`,(err)=>{
+                if(err) handleEventError(res);
+            });
+            body = [];
+        });
+    }
+};
+
+const handleEventError = res => {
+    res.writeHead(404,{'Location':'/'});
+    res.write('404 Not Found');
+    return res.end();
+}
 
 module.exports = handleRequestListener;
